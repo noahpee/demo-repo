446c18a5114e4d7aa9f3ecf414f7d5c347421851
noahpee <junglebook.noah@gmail.com>
2023-04-03 11:23:11 +0100

    fuck these stupid functions

--- js/app.js
+++ js/app.js
@@ -8,12 +8,46 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b) { 
+
+    const array1 =[]
+
+    const initialValue = 0;
+
+    let sum = 0
+
+    let sumAnswer = 'The sum of '
+
+    for (let i = 0; i < arguments.length; i++) {
+
+        array1.push(arguments[i])
+
+        sum += arguments[i]
+
+        if (i != arguments.length -1) {
+
+            sumAnswer += arguments[i] + ' and '
+
+        } else {
+
+            sumAnswer += arguments[i] + ' is ' + sum + '.'
+
+        }
+
+    }
+    
+    const sumWithInitial = array1.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
+
+    console.log(sumWithInitial, sumAnswer);
+
+    return sum
+
 
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -26,12 +60,46 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+
+function multiply(a, b) { 
+
+    const array1 =[]
+
+    const initialValue = 1;
+
+    let sum = 1
+
+    let sumAnswer = 'The product of '
+
+    for (let i = 0; i < arguments.length; i++) {
+
+        array1.push(arguments[i])
+
+        sum = sum *= arguments[i]
+
+        if (i != arguments.length -1) {
+
+            sumAnswer += arguments[i] + ' and '
+
+        } else {
+
+            sumAnswer += arguments[i] + ' is ' + sum + '.'
+
+        }
+
+    }
+    
+    const sumWithInitial = array1.reduce((accumulator, currentValue) => accumulator * currentValue, initialValue);
+
+    console.log(sumWithInitial, sumAnswer);
+
+    return multiply
+
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -49,10 +117,23 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
+    let testArray = []
+
+    for (let i = 0; i < arguments.length; i++) {
+
+    sum.push(arguments[i])
+
+    console.log(testArray)
+
+    }
+
+    sum()
+
+    return sumAndMultiply
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
